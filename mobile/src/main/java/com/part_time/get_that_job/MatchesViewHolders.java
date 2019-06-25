fb7667b130477a1108a7a6f79923b39147714d46
Koez <ahmadfakhrul127@gmail.com>
2019-06-26 02:12:33 +0800

    Initial commit

---
+++ mobile/src/main/java/com/part_time/get_that_job/MatchesViewHolders.java
@@ -0,0 +1,41 @@
+package com.part_time.get_that_job;
+
+import android.content.Intent;
+import android.os.Bundle;
+//import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.part_time.get_that_job.ChatActivity;
+
+import androidx.recyclerview.widget.RecyclerView;
+//import com.part_time.get_that_job.ChatActivity;
+//import com.simcoder.tinder.R;
+
+/**
+ * Created by manel on 10/31/2017.
+ */
+
+public class MatchesViewHolders extends RecyclerView.ViewHolder implements View.OnClickListener{
+    public TextView mMatchId, mMatchName;
+    public ImageView mMatchImage;
+    public MatchesViewHolders(View itemView) {
+        super(itemView);
+        itemView.setOnClickListener(this);
+
+        mMatchId = (TextView) itemView.findViewById(R.id.Matchid);
+        mMatchName = (TextView) itemView.findViewById(R.id.MatchName);
+
+        mMatchImage = (ImageView) itemView.findViewById(R.id.MatchImage);
+    }
+
+    //@Override
+    public void onClick(View view) {
+        Intent intent = new Intent(view.getContext(), ChatActivity.class);
+        Bundle b = new Bundle();
+        b.putString("matchId", mMatchId.getText().toString());
+        intent.putExtras(b);
+        view.getContext().startActivity(intent);
+    }
+}
